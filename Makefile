58427501a6b7c33d9d86f4d23741ef7ad0932f69
sebasvega95 <sebas060495@gmail.com>
2017-05-11 14:21:31 -0500

    Using a hash table per thread

--- Makefile
+++ Makefile
@@ -2,7 +2,10 @@ CXX = g++
 CFLAGS = -std=c++11 -O2 -Wall
 CLIBS = -lpthread
 
-all: global-ht
+all: global-ht local-ht
 
 global-ht: global-ht.cpp
 	$(CXX) $(CFLAGS) global-ht.cpp $(CLIBS) -o global-ht.out
+
+local-ht: local-ht.cpp
+	$(CXX) $(CFLAGS) local-ht.cpp $(CLIBS) -o local-ht.out
---
+++ local-ht.cpp
@@ -0,0 +1,97 @@
+#include <getopt.h>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+
+#include "lib/file.hpp"
+
+void count_words(std::string &text,
+                 std::unordered_map<std::string, int> &freq) {
+  std::stringstream words(text);
+  std::string word;
+  while (words >> word) {
+    std::transform(word.begin(), word.end(), word.begin(), tolower);
+    word.erase(std::remove_if(word.begin(), word.end(), ispunct), word.end());
+    freq[word]++;
+  }
+}
+
+void usage(char *program_name) {
+  int n = 1;
+  std::string opts[] = {"-j, --threads"};
+  std::string description[] = {"Number of threads to use"};
+
+  std::cout << "Usage: " << program_name << " [options ...] text" << std::endl;
+  std::cout << std::endl;
+  std::cout << "Options" << std::endl;
+  for (int i = 0; i < n; i++) {
+    std::cout << "  " << opts[i] << ": " << description[i] << std::endl;
+  }
+
+  exit(EXIT_FAILURE);
+}
+
+void run_program(std::string filename, size_t num_threads) {
+  std::vector<std::thread> threads(num_threads);
+  std::vector<std::unordered_map<std::string, int>> local_freq(num_threads);
+  std::vector<std::string> text = file::read_word_chunks(filename, num_threads);
+
+  for (size_t i = 0; i < num_threads; i++) {
+    threads[i] =
+        std::thread(count_words, std::ref(text[i]), std::ref(local_freq[i]));
+  }
+
+  for (auto &th : threads) {
+    th.join();
+  }
+
+  std::unordered_map<std::string, int> freq;
+  for (auto &ht : local_freq) {
+    for (auto &elem : ht) {
+      std::string word = elem.first;
+      int count = elem.second;
+      freq[word] += count;
+    }
+  }
+
+  for (auto &elem : freq) {
+    std::string word = elem.first;
+    int count = elem.second;
+    std::cout << word << ": " << count << std::endl;
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int opt, opt_index = 0;
+  static struct option options[] = {{"threads", required_argument, 0, 'j'},
+                                    {0, 0, 0, 0}};
+
+  size_t num_threads = 1;
+  while ((opt = getopt_long(argc, argv, "j:", options, &opt_index)) != -1) {
+    switch (opt) {
+      case 'j':
+        num_threads = atoi(optarg);
+        break;
+      default:
+        usage(argv[0]);
+        break;
+    }
+  }
+
+  if (argc - optind != 1) {
+    std::cout << "Error: You must provide a text" << std::endl << std::endl;
+    usage(argv[0]);
+  }
+
+  std::string filename = argv[optind];
+  if (!file::exists(filename)) {
+    std::cout << "Error: Text file not found" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  run_program(filename, num_threads);
+  return 0;
+}
